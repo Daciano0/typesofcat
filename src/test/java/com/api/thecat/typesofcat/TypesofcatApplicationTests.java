d65d4356882cbe119706c22310c5fc7892e477b2
DACIANO.SANTOS <daciano.santos@b2wdigital.com>
2020-05-19 12:18:05 -0300

    fix

--- src/test/java/com/api/thecat/typesofcat/TypesofcatApplicationTests.java
+++ src/test/java/com/api/thecat/typesofcat/TypesofcatApplicationTests.java
@@ -2,12 +2,14 @@ package com.api.thecat.typesofcat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class TypesofcatApplicationTests {
 
 	@Test
 	void contextLoads() {
+		TypesofcatApplication.main(new String[] {});
 	}
 
 }
---
+++ src/test/java/com/api/thecat/typesofcat/service/TypesCatServiceTests.java
@@ -0,0 +1,26 @@
+package com.api.thecat.typesofcat.service;
+
+import com.api.thecat.typesofcat.domain.CatBreeds;
+import com.api.thecat.typesofcat.repository.TypesCatRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+public class TypesCatServiceTests {
+
+
+
+}
